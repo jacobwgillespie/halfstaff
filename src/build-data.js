3895c5727195357b91c7de29a6e49e684dbbfc97
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-06-17 10:32:44 -0500

    Initial commit

---
+++ src/build-data.js
@@ -0,0 +1,112 @@
+/* eslint-disable no-console */
+
+import fs from 'fs-extra';
+import moment from 'moment';
+import path from 'path';
+
+import potus from '../data/potus.json';
+
+const flatten = list => list.reduce(
+  (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
+);
+
+const basePath = path.join(__dirname, '..', 'data', 'potus');
+const months = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december',
+];
+const endTimes = '(noon|sunset)';
+const date = `(this )?(${months.join('|')}) \\d{1,2},? \\d{4}`;
+const relativeDate = '(on (the|this)( \\w+)? day( of (interment))?)';
+const untilRegex = new RegExp(`(until|through)( ${endTimes})?,? ((${date})|${relativeDate})`, 'i');
+
+const tagProclamation = proclamation => {
+  const textBody = proclamation.body.map(
+    text => text.replace(/\s/, ' ').replace(/[ ]{2,}/, ' ').trim()
+  ).join(' ');
+
+  const tags = {};
+
+  if (textBody.match(/on the day of interment/i)) {
+    tags.type = 'interment';
+    tags.person = (textBody.match(/death of (\w+ \w+)/i) || [])[1];
+    if (tags.person) {
+      tags.person = textBody.match(new RegExp(`${tags.person}`, 'ig'));
+      tags.person = tags.person[tags.person.length - 1];
+    }
+  } else {
+    tags.type = 'date';
+  }
+
+  const durations = textBody.match(untilRegex);
+
+  let newestDate = moment.utc(proclamation.date);
+  proclamation.nlp.forEach(nlp => {
+    const nlpDate = moment.utc(nlp.date);
+    if (nlpDate.isAfter(newestDate)) newestDate = nlpDate;
+  });
+  tags.newestDate = newestDate.valueOf();
+
+  if (durations && durations.length > 0) {
+    tags.startDate = proclamation.date;
+    if (durations[1] === 'through') {
+      tags.endTime = 'end_of_day';
+    } else {
+      tags.endTime = durations[3];
+    }
+
+    if (durations[5]) {
+      tags.endDate = moment.utc(durations[5], 'MMMM D, YYYY').valueOf();
+    }
+  } else {
+    tags.startDate = tags.endDate = newestDate.valueOf();
+    tags.endTime = 'end_of_day';
+  }
+
+  return {
+    ...proclamation,
+    tags,
+  };
+};
+
+const buildHTML = proclamation => {
+  const normalizedBody = flatten(proclamation.body.map(
+    line => line.replace(/[\t\n]/, ' ').split(/<br( ?\/)?>/)
+  ));
+
+  const html = normalizedBody.filter(
+    line => line
+  ).map(line => {
+    const trimmedLine = line.trim();
+
+    if (trimmedLine.match(/[a-z]/)) {
+      return `<p>${trimmedLine}</p>`;
+    }
+    return `<p class="center">${trimmedLine}</p>`;
+  }).join('');
+
+  return {
+    ...proclamation,
+    html,
+  };
+};
+
+const proclamations = potus.map(
+  proclamation => tagProclamation(proclamation)
+).map(
+  proclamation => buildHTML(proclamation)
+);
+
+proclamations.forEach(proclamation => {
+  fs.writeJson(path.join(basePath, `${proclamation.id}.json`), proclamation);
+});
---
+++ src/fetch-potus.js
@@ -0,0 +1,85 @@
+/* eslint-disable no-console */
+
+import buildXray from 'x-ray';
+import crypto from 'crypto';
+import Hashids from 'hashids';
+import Knwl from 'knwl.js';
+import moment from 'moment';
+
+const hashids = new Hashids('halfstaff');
+
+const flatten = list => list.reduce(
+  (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
+);
+
+const xInstance = buildXray();
+const x = (...args) => new Promise((resolve, reject) => {
+  xInstance(...args)((err, data) => {
+    if (err) reject(err);
+    else resolve(data);
+  });
+});
+
+const fetchProclamationLinks = (page = null) => {
+  const query = page ? `?page=${page}` : '';
+  const url = `https://www.whitehouse.gov/briefing-room/presidential-actions/proclamations${query}`;
+
+  return x(url, ['h3 a@href']);
+};
+
+const fetchProclamation = url =>
+  x(url, '#page', {
+    title: '.pane-node-title h1',
+    date: '.press-article-date',
+    body: ['#content-start .field-items p@html'],
+  }).then(
+    data => ({ ...data, url, body: data.body.map(p => p.trim()) })
+  );
+
+const fetchFlagProclamations = async urls => {
+  const proclamations = await Promise.all(urls.map(url => fetchProclamation(url)));
+  return proclamations.filter(
+    proclamation => proclamation.body.find(
+      text => text.match(/half[\s-]staff/)
+    )
+  ).map(
+    proclamation => {
+      const knwl = new Knwl('english');
+      knwl.init(proclamation.body.join(' '));
+
+      const date = moment.utc(proclamation.date, 'MMMM D, YYYY').valueOf();
+      const contentHash = crypto.createHash('md5').update(proclamation.title, 'utf8').digest('hex');
+      const id = hashids.encodeHex(date.toString(16), contentHash);
+
+      return {
+        ...proclamation,
+        id,
+        date,
+        nlp: knwl.get('dates').map(nlp => ({
+          ...nlp,
+          date: moment.utc({ year: nlp.year, month: nlp.month - 1, day: nlp.day }).valueOf(),
+        })),
+      };
+    }
+  );
+};
+
+(async () => {
+  const allProclamations = flatten(await Promise.all([
+    fetchProclamationLinks(),
+    fetchProclamationLinks(1),
+    fetchProclamationLinks(2),
+    fetchProclamationLinks(3),
+    fetchProclamationLinks(4),
+    fetchProclamationLinks(5),
+    fetchProclamationLinks(6),
+    fetchProclamationLinks(7),
+    fetchProclamationLinks(8),
+    fetchProclamationLinks(9),
+    fetchProclamationLinks(10),
+  ]));
+
+  const flagProclamations = await fetchFlagProclamations(allProclamations);
+
+  console.log(JSON.stringify(flagProclamations));
+})();
