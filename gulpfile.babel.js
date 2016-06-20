6cdd4f7084ea8c99d0cad8cb78fd15fe386107de
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-06-20 16:42:53 -0500

    Fix serve task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -155,7 +155,7 @@ gulp.task('serve', ['scripts', 'styles'], () => {
 
   gulp.watch(['app/**/*.html'], reload);
   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
+  gulp.watch(['app/scripts/**/*.js'], ['scripts', reload]);
   gulp.watch(['app/images/**/*'], reload);
 });
 
