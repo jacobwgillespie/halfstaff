b101bd5476c0d2f3ebff4fccd32cbab712930c20
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-07-10 20:38:51 -0500

    Use redis for session storage

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module Halfstaff
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    if ENV['REDIS_URL']
+      config.cache_store = :redis_store, "#{ENV['REDIS_URL']}/0/cache", { expires_in: 90.minutes }
+    end
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_halfstaff_session'
+if ENV['REDIS_URL']
+  Rails.application.config.session_store :redis_store, servers: "#{ENV['REDIS_URL']}/0/session"
+else
+  Rails.application.config.session_store :cookie_store, key: '_halfstaff_session'
+end
